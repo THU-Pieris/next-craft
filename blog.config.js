cece52a1c4b8f53ef7acfbe6fe4279f72f89ef80
THU-Pieris <yogabear123456@gmail.com>
2022-05-31 16:08:33 +0800

    stable version v0.1.0

--- blog.config.js
+++ blog.config.js
@@ -1,17 +1,17 @@
 const BLOG = {
-  title: 'NextCraft',
-  author: 'Zuo Lan',
-  link: 'https://next-craft.vercel.app',
-  description: 'Using Craft.do pages as a blog',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW']
+  title: 'Pieris的博客',
+  author: 'Pieris',
+  link: 'https://pieris-craft.vercel.app',
+  description: 'Powered by Craft.do.',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#F2F2F2', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#212936', // use hex value, don't forget '#'
-  since: 2021, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-zl.vercel.app', // The link to generate OG image, don't end with a slash
-  craftConfigShareUrl: 'https://www.craft.do/s/8gQSdBtbuPjpp1', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/SCJBPj9t3tAWoR', // The link to share your craft config
   notesNav: {
     index: 'Home',
     blog: 'My Blog',
@@ -19,13 +19,13 @@ const BLOG = {
   },
   notesLink: {
     index: '/',
-    blog: 'https://zuolan.me',
-    contact: 'https://zuolan.me/contact'
+    blog: 'https://archive.pieris05.com',
+    contact: 'https://pieris05.com/#contact'
   },
   socialLink: {
-    twitter: 'https://twitter.com/izuolan',
-    github: 'https://github.com/izuolan',
-    telegram: 'https://t.me/zuolan'
+    twitter: 'https://twitter.com/JiaxiongYu',
+    github: 'https://github.com/THU-Pieris',
+    telegram: 'https://t.me/pieris_bot'
   },
   seo: {
     keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
